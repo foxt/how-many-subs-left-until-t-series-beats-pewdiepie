1c53b2cfabc02a6d2da5808f49006b3e442b9c57
Leo Nesfield <leo@thelmgn.com>
2019-03-14 23:09:43 +0000

    deeto

--- js/index.js
+++ js/index.js
@@ -30,22 +30,6 @@ var oldT = 0
 var gotPew = false
 var gotT = false
 
-String.prototype.toHHMMSS = function () {
-  var delta = new Number(this)
-  var days = Math.floor(delta / 86400);
-  delta -= days * 86400;
-  
-  // calculate (and subtract) whole hours
-  var hours = Math.floor(delta / 3600) % 24;
-  delta -= hours * 3600;
-  
-  // calculate (and subtract) whole minutes
-  var minutes = Math.floor(delta / 60) % 60;
-  delta -= minutes * 60;
-  
-  return days + "d " + hours + "h " + minutes + "m"
-}
-
 function checkChannel(channel,callback) {
   try {
     var xhttp = new XMLHttpRequest();
